daebed06fa212ef2c1160d08f9da5a0d1961e357
Nyx <nichole.steig@gmail.com>
2019-12-05 15:28:59 -0800

    added routes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -5,4 +5,9 @@ module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
         res.json(notesData);
     });
+
+    app.post("/api/notes", function(req, res) {
+        notesData.push(req.body);
+        res.json(true);
+    });
 }
